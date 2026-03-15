bdbce5afb14a14abf0cfc87366a18ccb318ecc01
frisk-scratch <yuniaochaoyang@gmail.com>
2026-03-15 19:47:18 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ function createWindow() {
         width: 1000,
         height: 700,
         webPreferences: {
-            preload: path.join(__dirname, "preload.ts"),
+            preload: path.join(__dirname, "preload.js"),
             nodeIntegration: false,
             contextIsolation: true
         }
