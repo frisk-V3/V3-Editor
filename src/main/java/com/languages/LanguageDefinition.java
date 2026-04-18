a3b46b87da39bb93883fedf1869199b43602e395
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-19 07:45:17 +0900

    Delete src/main directory

--- src/main/java/com/languages/LanguageDefinition.java
+++
@@ -1,9 +0,0 @@
-package com.languages;
-
-import java.util.List;
-
-public class LanguageDefinition {
-    public String name;                 // 言語名
-    public List<String> extensions;     // ["cs", "js", "py"]
-    public List<TokenRule> tokens;      // ハイライトルール
-}
--- src/main/java/com/languages/TokenRule.java
+++
@@ -1,7 +0,0 @@
-package com.languages;
-
-public class TokenRule {
-    public String name;     // 例: "keyword"
-    public String pattern;  // 正規表現
-    public String color;    // #RRGGBB
-}
